b1af51b86dddb6681d3bec3a44bec83730c2f5c5
zlrkw11 <ray040406@gmail.com>
2023-06-30 15:26:58 +1200

    guild member age fetch

--- bot/test bot 1.0 (hello-bot)/app.js
+++ bot/test bot 1.0 (hello-bot)/app.js
@@ -24,11 +24,12 @@ Client.on("ready", (client) => {
 });
 
 
-Client.login("MTEyMjQ5OTIzMzUzOTQ0NDc3Ng.GCv0ST.z-PQLKMnQx_TeWWnK-CJDzDibPv03k8sBum3Sw");
+Client.login("MTEyMjQ5OTIzMzUzOTQ0NDc3Ng.G4YYN4.Ug2-O3sUR5_lRE4SytCtQN_1ZlNsI8cjyP2sPo");
 
-//L49 messages
+//when a message is created/posted
 Client.on("messageCreate", (message)=>{
    //lowercase message content
+   const userInputText = message.content.toLowerCase();
 
    //only msg by user will have the tick
    if (message.author.bot) {return};
@@ -37,6 +38,47 @@ Client.on("messageCreate", (message)=>{
 
    //only allow non-bots to perform any code execution
    if (message.author.bot == false) {
-      message.reply("Hello world! You're not a bot! STFU!")
+      //message.reply("Hello world! You're not a bot! STFU!")
    }
+
+   //commands
+   if (userInputText == "!commands" || userInputText == "!help"){
+      message.reply("This bot operates on the following commands: !help !commans !age !math")
+   }
+
+   //basic math functions
+   if(userInputText == "!math"){
+      message.reply("5 + 2 - 1 * 5/2 - 4 + 7 * 3 % 5 = " + (5 + 2 - 1 * 5/2 - + 7 * 3 % 5));
+   }
+
+   //server age and member age in server
+   if (userInputText == "!age") {
+      //console.log(message.channel)
+      console.log(message.guild.createdTimestamp); //ms since 1970/01/01
+      console.log(message.guild.createdAt); //date when server created
+      message.reply("Server was created at " + message.guild.createdAt.toString()); //date to string
+      console.log(new Date(message.guild.createdTimestamp).toString()); //converted to actual date
+
+   
+
+   //fetch a guild members returns a promise.
+      message.guild.members.fetch().then(
+         (value) => {
+            //console.log(value);
+            //print the id of the author of msg
+            value.forEach(user => { //user = guild.member     2nd user = property   id=class
+               console.log(user.user.id + " " + message.author.id); //every user + author user
+               console.log(user.joinedTimestamp); //print joined timestamp
+               let date = new Date(user.joinedTimestamp); //ms --> date first
+
+               message.reply(user.displayName + " joined at " + date.toString()); //date --> string
+            });
+         }, 
+         (error) => {
+            console.log(error);
+         }
+      );
+
+   };
+   
 });
